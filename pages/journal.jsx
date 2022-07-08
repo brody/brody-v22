acb506f1b32a759659bf2e141dff61bb6d086151
Brody MacLean <brody.maclean@t4cjcmy0d7.lan>
2022-07-08 21:22:28 +1000

    update journal

--- pages/journal.jsx
+++ pages/journal.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function About() {
   return (
     <div className='flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6'>
-      <h2>About - Coming soon</h2>
+      <h2>Journal - Coming soon</h2>
     </div>
   );
 }
